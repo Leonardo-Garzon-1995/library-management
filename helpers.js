f4fd08120def26958511ba6897377a11b3d011a8
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2025-12-31 08:00:16 +0100

    add: 'list-users' to listOfCommands

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
 
 export const listOfCommands = () => {
-    const commands = ['add-book', 'add-user', 'borrow-book', 'return-book', 'list-books'];
+    const commands = ['add-book', 'add-user', 'borrow-book', 'return-book', 'list-books', "list-users"];
 
     console.log('Available commands: \n');
     for (const command of commands) {
